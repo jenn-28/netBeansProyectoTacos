7e6107d83bfbef4da26d670252a2151d4f9fc6c8
thnlink707 <monsivais_th@outlook.com>
2025-12-06 15:09:08 -0600

    feat: Modelado de Insumo y agregado al almacen de datos

---
+++ src/modelo/AlmacenDatos.java
@@ -0,0 +1,10 @@
+package modelo;
+
+import estructuras.ListaEnlazada;
+import estructuras.Pila;
+
+public class AlmacenDatos {
+    //LISTAS GLOBALES (Simulan una base de datos)
+    //lista de insumos
+    public static ListaEnlazada<Insumo> listaInsumos = new ListaEnlazada<>();
+}
